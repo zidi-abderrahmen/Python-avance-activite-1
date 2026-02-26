2bc63c3009d92ec9304b218f8af7d1a590e58e25
zidiabderrahmen <rhoumazaza@gmail.com>
2026-02-26 17:04:15 +0100

    Update: Updating the 'main.py' and adding the 'update_item' function

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+	name: str
+	price: float
+	is_offer: bool | None = None
 
 @app.get("/")
 def read_root():
@@ -9,3 +15,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
 	return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+	return {"item_name": item.name, "item_id": item_id}
